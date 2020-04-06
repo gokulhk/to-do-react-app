316902dbe35957397e630a490dd31cf28040643f
gokulhk <gokulhk147@gmail.com>
2020-04-06 21:14:32 +0530

    default task container display

---
+++ src/components/EmptyComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Card } from 'react-bootstrap'
+
+function EmptyComponent() {
+    return (
+        <div> <br />
+            <Card >No Cards to Display</Card>
+        </div>
+    )
+}
+
+export default EmptyComponent
