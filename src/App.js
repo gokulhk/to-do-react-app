58721eaf66c82659558d6fd9355f05794d4256a0
gokulhk <gokulhk147@gmail.com>
2020-04-06 21:05:57 +0530

    dependency and config files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css'
+
+//Redux Store imports
+import { Provider } from 'react-redux'
+import store from './redux/tasks/taskStore'
+
+//Bootstrap imports
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card } from 'react-bootstrap'
+
+//Component imports
 import HeaderComp from './components/HeaderComp';
 import AddTaskComp from './components/AddTaskComp';
+import TaskContainer from './components/TaskContainer'
+
+
 
 function App() {
 
   return (
-    <div className="App">
-      <div className="container-fluid">
-        <HeaderComp />
-        <AddTaskComp /> 
-      </div>
+    <Provider store={store}>
+      <div className="App container"><br />
 
-    </div>
+        <HeaderComp />
+
+        <Card className="card border border-white">
+
+          <Card className="card border border-white" >
+            <TaskContainer />
+          </Card><br />
+
+          <Card className="card"><br />
+            <AddTaskComp />
+          </Card>
+
+        </Card>
+      </div>
+    </Provider>
   );
 
 
