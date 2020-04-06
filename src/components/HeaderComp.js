955dbb7018e59ded79db11549c1395b1d07a4aa9
gokulhk <gokulhk147@gmail.com>
2020-04-06 21:13:36 +0530

    displays application header

--- src/components/HeaderComp.js
+++ src/components/HeaderComp.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
+//Bootstrap imports
+import { Card } from 'react-bootstrap'
+
 function HeaderComp() {
     return (
-        <div>
-            <h1 className="header">To Do Application</h1>
-        </div>
+        <Card body>
+            <h3 className="header" >React - To Do Application</h3>
+        </Card>
     )
 }
 
