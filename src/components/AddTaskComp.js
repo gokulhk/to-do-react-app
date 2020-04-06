ab82a441e7f6ea87595d454c000a9c90a93ac17d
gokulhk <gokulhk147@gmail.com>
2020-04-06 21:12:50 +0530

    feature to add new tasks

--- src/components/AddTaskComp.js
+++ src/components/AddTaskComp.js
@@ -1,36 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function AddTaskComp() {
+//Redux imports
+import { connect } from 'react-redux'
+import { addTaskContainerOne } from '../redux/index'
+
+//Bootstrap imports
+import 'bootstrap/dist/css/bootstrap.css'
+
+function AddTaskComp(props) {
+
+    //taskId counter 
+    const [ taskId, setTaskId ] = useState(3)
+
+    //destructure props
+    const { addTaskContainerOne } = props
+
 
     //creating ref 
     const taskTitleRef = React.createRef()
-    const taskDescRef = React.createRef()
 
-    const submitHandler = () => {
+    const submitHandler = event => {
+
+
+
+        //prevents refresh onSubmit event
+        event.preventDefault()
 
         //get input values
-        const taskTitle = taskTitleRef.current.value
-        const taskDescription = taskDescRef.current.value
+        const taskTitle = taskTitleRef.current.value;
 
-        alert(`Title : ${taskTitle} \n Task Description : ${taskDescription}`)
+
+        //empty entry rejection
+        if(taskTitle === ''){
+            taskTitleRef.current.focus()
+            return
+        }
+        
+        //clear input
+        taskTitleRef.current.value = '';
+
+        //create new Task object
+        const newTask = {
+            taskId,
+            taskTitle,
+            taskContainerId : 1
+        }
+
+        //increment taskId 
+        setTaskId(taskId + 1);
+
+        addTaskContainerOne(taskId, newTask)
 
     }
 
     return (
-        <form>
-            <div>
-            <label>Task Title</label><br />
-            <input  ref={taskTitleRef} placeholder="Enter title..."/>
+        <form className="form-horizontal">
+
+            <div className="form-group row">
+                <label className="control-label col-sm-2">Task Title</label>
+                <div className="col-sm-10">
+                    <input ref={taskTitleRef} type="text" className="form-control" 
+                    placeholder="Eg : Jogging @ 5AM tommorrow" />
+                </div>
             </div>
 
-            <div>
-            <label>Task Description</label><br />
-            <textarea  ref={taskDescRef} placeholder="Enter task description..." />
-            </div>
-
-            <button className="btn btn-success" type="submit" onClick={submitHandler}>Add Task</button>
+            <button type="submit"
+                className="btn btn-success"
+                onClick={submitHandler}>Add Task</button>
         </form>
+
     )
 }
 
-export default AddTaskComp
+//step1:  mapDispatchToProps
+const mapDispatchToProps = dispatch => {
+    return {
+        addTaskContainerOne: (taskId, newTask) => dispatch(addTaskContainerOne(taskId, newTask))
+    }
+}
+
+//step 2 : connect react component and redux store
+export default connect(null, mapDispatchToProps)(AddTaskComp)
